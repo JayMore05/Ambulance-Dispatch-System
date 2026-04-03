883d2af3a8ab6d1196ead51a667ffa7305aceb88
JayMore05 <jaymore2156@gmail.com>
2026-04-03 17:58:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const frontendPath = path.join(__dirname, "../frontend");
+const frontendPath = path.join(__dirname, "frontend");
 app.use(express.static(frontendPath));
 
 app.get("/", (req, res) => res.sendFile(path.join(frontendPath, "index.html")));
